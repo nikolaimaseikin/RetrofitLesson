5830842cfd37b19ed743aa24886b724c57d22355
maseikin.nv <nikolaimaseikin@mail.ru>
2023-12-20 10:05:36 +0300

    second

--- app/src/main/java/com/example/retrofitlesson/MainActivity.kt
+++ app/src/main/java/com/example/retrofitlesson/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.retrofitlesson
 
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofitlesson.adapter.ProductAdapter
 import com.example.retrofitlesson.databinding.ActivityMainBinding
 import com.example.retrofitlesson.retrofit.AuthRequest
 import com.example.retrofitlesson.retrofit.LoginApi
+import com.example.retrofitlesson.retrofit.User
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -41,13 +45,32 @@ class MainActivity : AppCompatActivity() {
         val mainApi = retrofit.create(LoginApi::class.java)
 
 
+        var user: User? = null
         CoroutineScope(Dispatchers.IO).launch {
-            val productObj = mainApi.getAllProducts()
-            runOnUiThread {
-                binding.apply {
-                    adapter.submitList(productObj.products)
-                }
-            }
+            user = mainApi.auth(AuthRequest("kminchelle", "0lelplR"))
         }
+
+
+        binding.searchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val productObj = newText?.let {
+                        mainApi.getProductsByName(user?.token ?: "", it)
+                    }
+                    runOnUiThread {
+                        binding.apply {
+                            adapter.submitList(productObj?.products)
+                        }
+                    }
+                }
+                return true
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/retrofitlesson/retrofit/LoginApi.kt
+++ app/src/main/java/com/example/retrofitlesson/retrofit/LoginApi.kt
@@ -3,8 +3,11 @@ package com.example.retrofitlesson.retrofit
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface LoginApi {
     @GET("products/{id}")
@@ -15,4 +18,10 @@ interface LoginApi {
 
     @GET("products")
     suspend fun getAllProducts(): Products
+
+    @Headers(
+        "ContentType: application/json"
+    )
+    @GET("products/search")
+    suspend fun getProductsByName(@Header("Authorization") token: String, @Query("q") name: String): Products
 }
